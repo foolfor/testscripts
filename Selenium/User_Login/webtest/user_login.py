67a788106f2a580d703449dc0df68d7744d4a535
foolfor <64899331+foolfor@users.noreply.github.com>
2023-05-17 11:09:24 +0800

    compact

--- Selenium/User_Login/webtest/user_login.py
+++ Selenium/User_Login/webtest/user_login.py
@@ -13,31 +13,28 @@ class UserLogin:
     def login_only(self, user_name, user_password):
         """Do something after login.
         """
+        # get url
         self.driver.get(GlobalVals.URL)
         time.sleep(5)
 
+        # username
         self.driver.find_element(By.XPATH, GlobalVals.USER_XPATH).send_keys(user_name)
         time.sleep(2)
 
+        # password
         self.driver.find_element(By.XPATH, GlobalVals.PASS_XPATH).send_keys(user_password)
         time.sleep(2)
+
+        # submit
         self.driver.find_element(By.XPATH, GlobalVals.SUBMIT_XPATH).click()
         time.sleep(3)
 
     def login_return_value(self, user_name, user_password):
         """Login test
         """
-        self.driver.get(GlobalVals.URL)
-        time.sleep(5)
+        self.login_only(user_name, user_password)
 
-        self.driver.find_element(By.XPATH, GlobalVals.USER_XPATH).send_keys(user_name)
-        time.sleep(2)
-
-        self.driver.find_element(By.XPATH, GlobalVals.PASS_XPATH).send_keys(user_password)
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, GlobalVals.SUBMIT_XPATH).click()
-        time.sleep(3)
-
+        # find an element of the new page
         info_assert = self.driver.find_element(By.XPATH, GlobalVals.LOGO_XPATH)
         value = info_assert.get_attribute("innerHTML")
         time.sleep(2)
